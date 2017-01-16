da7b1d32ea49b557e4e5f8627edc457424fb5971
Milovan Minic <milovan.minic@gmail.com>
2017-01-16 20:19:01 +0100

    Configured the sound effects for playback of recorded sound

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -25,7 +25,7 @@ class PlaySoundsViewController: UIViewController {
     var audioPlayerNode: AVAudioPlayerNode!
     var stopTimer: Timer!
     
-    enum buttonType: Int {
+    enum ButtonType: Int {
         case slow = 0
         case fast
         case highPitch
@@ -36,6 +36,23 @@ class PlaySoundsViewController: UIViewController {
     
     @IBAction func playSoundForButton(_ sender: UIButton) {
         print("Play sound button pressed")
+        
+        switch (ButtonType(rawValue: sender.tag)!) {
+        case .slow:
+            playSound(rate: 0.5)
+        case .fast:
+            playSound(rate: 1.5)
+        case .highPitch:
+            playSound(pitch: 1000)
+        case .lowPitch:
+            playSound(pitch: -1000)
+        case .echo:
+            playSound(echo: true)
+        case .reverb:
+            playSound(reverb: true)
+        }
+        
+        configureUI(.playing)
     }
     
     @IBAction func stopButtonPressed(_ sender: AnyObject) {
